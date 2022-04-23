0c60fca3ffc26dbea3a39c8039e7b641878e6fda
shobhit jaryal <jaryalshaurya2203@gmail.com>
2022-04-23 17:34:32 +0530

    fix token

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,33 +1,31 @@
 import React from "react";
-import { useEffect , useContext } from "react";
+import { useEffect, useContext } from "react";
 import Loader from "../layout/loading";
 import UserItem from "./UserItem";
 import GithubContext from "../../context/github/GithubContext";
 function UserResults() {
-
-    const {users,loading,fetchUser} = useContext(GithubContext)
+  const { users, loading, fetchUser } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUser();
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[]);
+  }, []);
 
-  if(!loading){
-      
-      return (
-        <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-          {users.map((user) => (
-            <UserItem key={user.id} user={user}/>
-          ))}
-          </div>
-        );
-}else{
-   return (
-       <div >
-           <Loader />
-       </div>
-   );
-}
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  }
 }
 
 export default UserResults;
